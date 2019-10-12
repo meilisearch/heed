5ffd62fe59c354e7b20906902748729fee392923
Clément Renault <renault.cle@gmail.com>
2019-10-12 15:59:53 +0200

    Expose iter types

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod txn;
 pub mod types;
 
 use self::cursor::{RoCursor, RwCursor};
-pub use self::db::Database;
+pub use self::db::{Database, RoIter, RwIter, RoRange, RwRange};
 pub use self::lmdb_error::Error as LmdbError;
 pub use self::env::{EnvOpenOptions, Env};
 pub use self::traits::{BytesEncode, BytesDecode};
