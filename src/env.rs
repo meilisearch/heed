1f8c2346bb5295b6cd2e45b98831194cea41a259
Clément Renault <clement@meilisearch.com>
2019-11-25 17:28:23 +0100

    Support windows for LMDB compaction

--- src/env.rs
+++ src/env.rs
@@ -2,7 +2,6 @@ use std::any::TypeId;
 use std::collections::hash_map::{Entry, HashMap};
 use std::ffi::CString;
 use std::fs::File;
-use std::os::unix::io::AsRawFd;
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use std::{ptr, sync};
@@ -304,7 +303,10 @@ impl Env {
         RoTxn::new(self.0.env)
     }
 
+    #[cfg(not(windows))]
     pub fn copy_to_path<P: AsRef<Path>>(&self, path: P, option: CompactionOption) -> Result<File> {
+        use std::os::unix::io::AsRawFd;
+
         let flags = if let CompactionOption::Enabled = option {
             ffi::MDB_CP_COMPACT
         } else {
@@ -315,6 +317,24 @@ impl Env {
         let fd = file.as_raw_fd();
 
         unsafe { lmdb_result(ffi::mdb_env_copyfd2(self.0.env, fd, flags))? }
+
+        Ok(file)
+    }
+
+    #[cfg(windows)]
+    pub fn copy_to_path<P: AsRef<Path>>(&self, path: P, option: CompactionOption) -> Result<File> {
+        use std::os::windows::io::AsRawHandle;
+
+        let flags = if let CompactionOption::Enabled = option {
+            ffi::MDB_CP_COMPACT
+        } else {
+            0
+        };
+
+        let file = File::create(path)?;
+        let handle = file.as_raw_handle();
+
+        unsafe { lmdb_result(ffi::mdb_env_copyfd2(self.0.env, handle, flags))? }
 
         Ok(file)
     }
