8cecdb3d9fe75ada426f7bd94a1ffcf1cded85fb
Clément Renault <clement@meilisearch.com>
2020-08-29 14:44:15 +0200

    Introduce a method to change the types of a uniform database

--- heed/src/db/uniform.rs
+++ heed/src/db/uniform.rs
@@ -920,6 +920,50 @@ impl<KC, DC> Database<KC, DC> {
         self.dyndb.clear(txn)
     }
 
+    /// Change the codec types of this uniform database, specifying the codecs.
+    ///
+    /// # Safety
+    ///
+    /// It is up to you to ensure that the data read and written using the polymorphic
+    /// handle correspond to the the typed, uniform one. If an invalid write is made,
+    /// it can corrupt the database from the eyes of heed.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use std::fs;
+    /// # use std::path::Path;
+    /// # use heed::EnvOpenOptions;
+    /// use heed::{Database, PolyDatabase};
+    /// use heed::types::*;
+    /// use heed::{zerocopy::I32, byteorder::BigEndian};
+    ///
+    /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// # fs::create_dir_all(Path::new("target").join("zerocopy.mdb"))?;
+    /// # let env = EnvOpenOptions::new()
+    /// #     .map_size(10 * 1024 * 1024 * 1024) // 10GB
+    /// #     .max_dbs(3000)
+    /// #     .open(Path::new("target").join("zerocopy.mdb"))?;
+    /// type BEI32 = I32<BigEndian>;
+    ///
+    /// let db: Database<Unit, Unit> = env.create_database(Some("iter-i32"))?;
+    ///
+    /// let mut wtxn = env.write_txn()?;
+    /// # db.clear(&mut wtxn)?;
+    /// // We remap the types for ease of use.
+    /// let db = db.remap_types::<OwnedType<BEI32>, Str>();
+    /// db.put(&mut wtxn, &BEI32::new(42), "i-am-forty-two")?;
+    /// db.put(&mut wtxn, &BEI32::new(27), "i-am-twenty-seven")?;
+    /// db.put(&mut wtxn, &BEI32::new(13), "i-am-thirteen")?;
+    /// db.put(&mut wtxn, &BEI32::new(521), "i-am-five-hundred-and-twenty-one")?;
+    ///
+    /// wtxn.commit()?;
+    /// # Ok(()) }
+    /// ```
+    pub fn remap_types<KC2, DC2>(&self) -> Database<KC2, DC2> {
+        Database::new(self.dyndb.dbi)
+    }
+
     /// Get an handle on the internal polymorphic database.
     ///
     /// Using this method is useful when you want to skip deserializing of the value,
