0b9a284f9af242d98f679bd683987124a9833508
mpostma <postma.marin@protonmail.com>
2020-12-31 01:01:38 +0100

    change byte decode from option to result

--- heed-traits/src/lib.rs
+++ heed-traits/src/lib.rs
@@ -1,13 +1,14 @@
 use std::borrow::Cow;
+use std::error::Error;
 
 pub trait BytesEncode<'a> {
     type EItem: ?Sized + 'a;
 
-    fn bytes_encode(item: &'a Self::EItem) -> Option<Cow<'a, [u8]>>;
+    fn bytes_encode(item: &'a Self::EItem) -> Result<Cow<'a, [u8]>, Box<dyn Error>>;
 }
 
 pub trait BytesDecode<'a> {
     type DItem: 'a;
 
-    fn bytes_decode(bytes: &'a [u8]) -> Option<Self::DItem>;
+    fn bytes_decode(bytes: &'a [u8]) -> Result<Self::DItem, Box<dyn Error>>;
 }
