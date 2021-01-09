03b54a4e6dfe987d7bc22d01b86e40ca70c66aa1
Clément Renault <clement@meilisearch.com>
2021-01-09 19:40:17 +0100

    Add documentation to the CustomKeyCmp trait

--- heed-traits/src/lib.rs
+++ heed-traits/src/lib.rs
@@ -13,6 +13,18 @@ pub trait BytesDecode<'a> {
     fn bytes_decode(bytes: &'a [u8]) -> Option<Self::DItem>;
 }
 
+/// Define a custom key comparison function for a database.
+///
+/// The comparison function is called whenever it is necessary to compare a key specified
+/// by the application with a key currently stored in the database. If no comparison function
+/// is specified, and no special key flags were specified, the keys are compared lexically,
+/// with shorter keys collating before longer keys.
 pub trait CustomKeyCmp {
+    /// Compares the raw bytes representation of two keys.
+    ///
+    /// # Safety
+    ///
+    /// This function must never crash, this is the reason why it takes raw bytes as parameter,
+    /// to let you define the recovery method you want in case of a decoding error.
     fn compare(a: &[u8], b: &[u8]) -> Ordering;
 }
