bc8ba965af8f182a26da482fd1e90ac96c842504
Kerollmops <clement@meilisearch.com>
2023-01-14 19:19:28 +0100

    Rename the Flag enum and the Mdb prefixes

--- heed/src/mdb/lmdb_flags.rs
+++ heed/src/mdb/lmdb_flags.rs
@@ -3,16 +3,16 @@ use lmdb_master_sys as ffi;
 /// LMDB flags (see <http://www.lmdb.tech/doc/group__mdb__env.html> for more details).
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
-pub enum Flags {
-    MdbFixedmap = ffi::MDB_FIXEDMAP,
-    MdbNoSubDir = ffi::MDB_NOSUBDIR,
-    MdbNoSync = ffi::MDB_NOSYNC,
-    MdbRdOnly = ffi::MDB_RDONLY,
-    MdbNoMetaSync = ffi::MDB_NOMETASYNC,
-    MdbWriteMap = ffi::MDB_WRITEMAP,
-    MdbMapAsync = ffi::MDB_MAPASYNC,
-    MdbNoTls = ffi::MDB_NOTLS,
-    MdbNoLock = ffi::MDB_NOLOCK,
-    MdbNoRdAhead = ffi::MDB_NORDAHEAD,
-    MdbNoMemInit = ffi::MDB_NOMEMINIT,
+pub enum Flag {
+    Fixedmap = ffi::MDB_FIXEDMAP,
+    NoSubDir = ffi::MDB_NOSUBDIR,
+    NoSync = ffi::MDB_NOSYNC,
+    RdOnly = ffi::MDB_RDONLY,
+    NoMetaSync = ffi::MDB_NOMETASYNC,
+    WriteMap = ffi::MDB_WRITEMAP,
+    MapAsync = ffi::MDB_MAPASYNC,
+    NoTls = ffi::MDB_NOTLS,
+    NoLock = ffi::MDB_NOLOCK,
+    NoRdAhead = ffi::MDB_NORDAHEAD,
+    NoMemInit = ffi::MDB_NOMEMINIT,
 }
