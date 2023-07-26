f0eeabeadbf1137a71d395ef2f03bc73865e338c
Clément Renault <clement@meilisearch.com>
2023-07-26 15:05:39 +0200

    Rename and Document the MDB_NOTLS feature

--- heed/src/env.rs
+++ heed/src/env.rs
@@ -246,10 +246,10 @@ impl EnvOpenOptions {
                         mdb_result(ffi::mdb_env_set_maxdbs(env, dbs))?;
                     }
 
-                    // When the `send-read-txn` feature is enabled, we must force LMDB
+                    // When the `read-txn-no-tls` feature is enabled, we must force LMDB
                     // to avoid using the thread local storage, this way we allow users
                     // to use references of RoTxn between threads safely.
-                    let flags = if cfg!(feature = "send-read-txn") {
+                    let flags = if cfg!(feature = "read-txn-no-tls") {
                         self.flags | Flag::NoTls as u32
                     } else {
                         self.flags
