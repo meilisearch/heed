5bb58c1139a054268a4652131b9d4a129baf7357
Clément Renault <clement@meilisearch.com>
2023-11-28 16:38:48 +0100

    Make cargo fmt happy

--- lmdb-master-sys/bindgen.rs
+++ lmdb-master-sys/bindgen.rs
@@ -70,5 +70,7 @@ pub fn generate() {
         .generate()
         .expect("Unable to generate bindings");
 
-    bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
