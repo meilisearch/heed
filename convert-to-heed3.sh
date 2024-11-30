36a785b7df50e261c7c0f8af033f1c75ed00d1e0
Clément Renault <clement@meilisearch.com>
2024-11-30 15:43:42 +0100

    Fix the heed3 script on Linux

--- convert-to-heed3.sh
+++ convert-to-heed3.sh
@@ -14,7 +14,11 @@ cp heed3/Cargo.toml heed/Cargo.toml
 
 # ...and replaces the `heed::` string by the `heed3::` one.
 for file in $(find heed/src -type f -name "*.rs"); do
-    sed -i '' 's/heed::/heed3::/g' "$file"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' 's/heed::/heed3::/g' "$file"
+    else
+        sed -i 's/heed::/heed3::/g' "$file"
+    fi
 done
 
 echo "Heed3 crate setup completed successfully. Copied and modified configurations for the heed crate."
